7c1c13085bf82c2cddc42d960ef9021c572b3060
Bs1Bslteam <43539675+Bs1Bslteam@users.noreply.github.com>
2018-10-12 14:31:15 +0300

    !U
    Hotfix

--- WenCompiledNoError.data.js
+++ WenCompiledNoError.data.js
@@ -184,6 +184,6 @@ Module['FS_createPath']('/WenCompiledNoError/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 106, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"audio": 0, "start": 106, "crunched": 0, "end": 162, "filename": "/UE4CommandLine.txt"}, {"audio": 0, "start": 162, "crunched": 0, "end": 360, "filename": "/Engine/Build/Build.version"}, {"audio": 0, "start": 360, "crunched": 0, "end": 69245834, "filename": "/WenCompiledNoError/Content/Paks/WenCompiledNoError-HTML5.pak"}], "remote_package_size": 69245834, "package_uuid": "6551a8a9-aefb-4c63-99c6-5b8de844dca8"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 106, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"audio": 0, "start": 106, "crunched": 0, "end": 162, "filename": "/UE4CommandLine.txt"}, {"audio": 0, "start": 162, "crunched": 0, "end": 360, "filename": "/Engine/Build/Build.version"}, {"audio": 0, "start": 360, "crunched": 0, "end": 69245818, "filename": "/WenCompiledNoError/Content/Paks/WenCompiledNoError-HTML5.pak"}], "remote_package_size": 69245818, "package_uuid": "c59c80be-8592-423c-9116-358c8b2cf8f0"});
 
 })();
